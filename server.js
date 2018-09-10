67307ab927f13085958845b6c2369b2696d7f234
katherinestout <kjs14@hood.edu>
2018-09-10 11:34:08 -0400

    body parser

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require ('express');
 const app = express ();
+const bodyParser = require('body-parser');
+
+
+//body parser middleware
+/*side note: the url encoded method within bodyparser tells bodyparser to extract
+data from the form element and then add it to the body property in the REQUEST object*/
+app.use(bodyParser.urlencoded({extended: true}));
 
 //console.log('may the node be with you');
 
@@ -13,6 +20,9 @@ app.get('/', (req, res) => {
   app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')});
 
+    app.post('/submit', (req, res) => {
+        console.log(req.body)
+      });
 
 app.listen(3000, function(){
     console.log('listening on 3000')
