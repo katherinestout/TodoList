ae3430e02643ab8de9881ca04f1f92cf37e9b6a2
katherinestout <kjs14@hood.edu>
2018-09-10 19:47:11 -0400

    axios for Update Delete

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express ();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const ejs = require('ejs');
+const axios = require('axios');
 
+//setting ejs, dynamically generated
 app.set('view engine', 'ejs');
 //res.render(view, locals);
 
+//GET request
+//rendering the index.ejs file so it is dynamically generated
 app.get('/', (req, res) => {
     db.collection('items').find().toArray((err, result) =>{
 if (err) return console.log(err);
@@ -26,22 +30,14 @@ data from the form element and then add it to the body property in the REQUEST o
 app.use(bodyParser.urlencoded({extended: true}));
 
 //console.log('may the node be with you');
-/*
-app.get('/', (req, res) => {
-    res.send('Hello World')
-  }); */
 
+//GET request
 //getting the index file
   app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')});
 
-/*post route to the submit button
-    app.post('/submit', (req, res) => {
-        console.log(req.body)
-      });*/
-
-
-//post route to submit a to do item and using insertOne to save to the db
+//POST request
+//submit a to do item and using insertOne to save to the db
 app.post('/items', (req, res)=> {
     db.collection('items').insertOne(req.body, (err, result) =>
     {
@@ -51,7 +47,8 @@ app.post('/items', (req, res)=> {
     });
 });
 
-//
+//GET request
+//getting all of the todo items
 app.get('/', (req, res) => {
     const cursor = db.collection('items').find().toArray(function(err, results){
     console.log(results);
@@ -59,10 +56,7 @@ app.get('/', (req, res) => {
 });    
 
 
-
-
-
-//connection to mlab
+//connection to mlab, listening on PORT 3000
 MongoClient.connect('mongodb://katie:katiekatie1@ds161059.mlab.com:61059/ktcrudmeantodo', 
 (err, client)=> {
     if (err) return console.log(err);
