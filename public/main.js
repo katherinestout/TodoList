4c5e6e682ff6c3ee6829f72cc4e6c58414c4e2b2
katherinestout <kjs14@hood.edu>
2018-09-11 12:49:42 -0400

    delete functionality

---
+++ public/main.js
@@ -0,0 +1,35 @@
+//PUT request when the update button is clicked
+//const up = document.getElementById('update');
+const del= document.getElementById('delete');
+//const update = document.getElementById('update');
+/*
+update.addEventListener('click', function () {
+    fetch('quotes', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': 'Darth Vader',
+        'quote': 'I find your lack of faith disturbing.'
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+    })
+  }) */
+  
+  del.addEventListener('click', function () {
+    fetch('items', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'item': 'Walk the dog'
+      })
+    }).then(function (response) {
+      window.location.reload();
+    });
+  });
--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const app = express ();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const ejs = require('ejs');
-const axios = require('axios');
+//const axios = require('axios');
+
+
 
 //setting ejs, dynamically generated
 app.set('view engine', 'ejs');
-//res.render(view, locals);
+//body parser middleware
+/*side note: the url encoded method within bodyparser tells bodyparser to extract
+data from the form element and then add it to the body property in the REQUEST object*/
+app.use(bodyParser.urlencoded({extended: true}));
+//express.static middleware, tells Express to make public folder acessible to public
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
 //GET request
 //rendering the index.ejs file so it is dynamically generated
@@ -23,11 +31,6 @@ MongoClient.connect('link-to-mongodb', (err, database)=>{
     'mongodb://katie:katiekatie1@ds161059.mlab.com:61059/ktcrudmeantodo'
 });*/
 
-
-//body parser middleware
-/*side note: the url encoded method within bodyparser tells bodyparser to extract
-data from the form element and then add it to the body property in the REQUEST object*/
-app.use(bodyParser.urlencoded({extended: true}));
 
 //console.log('may the node be with you');
 
@@ -55,14 +58,29 @@ app.get('/', (req, res) => {
 });
 });    
 
-//express.static middleware, tells Express to make public folder acessible to public
-app.use(express.static('public'));
+/*
+app.put('/items', (req, res) => {
+    db.collection('items').findOneAndUpdate()
+}) */
+//app.updateOne('/items', ())
+
+app.delete('/items', (req, res) => {
+    db.collection('items').findOneAndDelete({item: req.body.item}, (err, result) => {
+      if (err) return res.send(500, err);
+     console.log('Your first item was deleted');
+     res.redirect('/');
+    });
+  });
+
+
+
 
 //connection to mlab, listening on PORT 3000
 MongoClient.connect('mongodb://katie:katiekatie1@ds161059.mlab.com:61059/ktcrudmeantodo', 
 (err, client)=> {
     if (err) return console.log(err);
     db = client.db('ktcrudmeantodo');
+
              
 app.listen(3000, () => {
     console.log('listening on 3000 may the node be with you');
